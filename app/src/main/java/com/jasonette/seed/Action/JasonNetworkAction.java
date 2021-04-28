ac20eb009988b26910696b7deb6277849dd6709c
Well-ington <celso.w.dias.a@gmail.com>
2021-04-28 10:26:34 -0300

    Feat: added file path reader/extractor in order to retrieve the file content as base64

--- app/src/main/java/com/jasonette/seed/Action/JasonNetworkAction.java
+++ app/src/main/java/com/jasonette/seed/Action/JasonNetworkAction.java
@@ -3,16 +3,22 @@ package com.jasonette.seed.Action;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
 import android.util.Base64;
 import android.util.Log;
 
-import com.jasonette.seed.Core.JasonViewActivity;
 import com.jasonette.seed.Helper.JasonHelper;
 import com.jasonette.seed.Launcher.Launcher;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.util.Iterator;
 import java.util.UUID;
@@ -27,13 +33,14 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 import timber.log.Timber;
 
+
+
 public class JasonNetworkAction {
     private void _request(final JSONObject callback, final JSONObject action, final JSONObject data, final JSONObject event, final Context context){
         try{
             final JSONObject options = action.getJSONObject("options");
             if(options.has("url")){
                 String url = options.getString("url");
-
                 // method
                 String method = "GET";
                 if(options.has("method")) {
@@ -64,7 +71,8 @@ public class JasonNetworkAction {
                         builder.addHeader(key, val);
                     }
                 }
-
+                JSONArray errorVal = new JSONArray();
+                JSONArray errorFullList = new JSONArray();
                 // Attach header passed in as options
                 if (options.has("header")) {
                     JSONObject header = options.getJSONObject("header");
@@ -124,7 +132,7 @@ public class JasonNetworkAction {
                         byte[] d;
                         if (content_type.equalsIgnoreCase("json")) {
                             mediaType = MediaType.parse("application/json; charset=utf-8");
-                            d = options.getString("data").getBytes();
+                           d = options.getString("data").getBytes();
                         } else {
                             mediaType = MediaType.parse(content_type);
                             d = Base64.decode(options.getString("data"), Base64.DEFAULT);
@@ -144,8 +152,82 @@ public class JasonNetworkAction {
                             try {
                                 while (keysIterator.hasNext()) {
                                     String key = (String) keysIterator.next();
-                                    String val = d.getString(key);
-                                    bodyBuilder.add(key, val);
+                                    try {
+                                        JSONArray valTest = d.getJSONArray(key);
+                                        JSONArray newVal = new JSONArray();
+
+                                        for (int i = 0; i < valTest.length(); i++) {
+                                            JSONObject row = valTest.getJSONObject(i);
+                                            JSONObject newRow = new JSONObject();
+                                            Boolean isFaulty = false;
+                                            Iterator<String> rowKeys = row.keys();
+                                            while(rowKeys.hasNext()) {
+
+                                                String rowKey = rowKeys.next();
+                                                String rowValue = row.getString(rowKey);
+                                                if (rowValue.startsWith("content://") || rowValue.startsWith("file://")) {
+                                                    Uri newFileUri = Uri.parse(rowValue);
+                                                        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+                                                    //Log.d("test", uri_for_session.toString() + session_domain.toString());
+                                                        if (Build.VERSION.SDK_INT < 29) {
+                                                            try {
+                                                                Bitmap bitmap = MediaStore.Images.Media.getBitmap(context.getContentResolver(), newFileUri);
+                                                                bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+                                                                byte[] byteArray = outputStream.toByteArray();
+                                                                String encodedString = Base64.encodeToString(byteArray, Base64.DEFAULT);
+                                                                newRow.put(rowKey, encodedString);
+                                                            } catch (SecurityException e) {
+                                                                newRow.put(rowKey, rowValue);
+                                                                JSONObject errorItem = new JSONObject();
+                                                                errorItem.put("_parent_id", row.getString("_id"));
+                                                                errorItem.put("_parent", key);
+                                                                errorItem.put("_id", errorVal.length());
+                                                                errorFullList = valTest;
+                                                                errorVal.put(errorItem);
+                                                                isFaulty = true;
+                                                            }
+                                                        } else {
+                                                            Log.d("ok then", "before parsing");
+                                                            try {
+                                                                context.grantUriPermission("com.google.android.apps.photos.contentprovider", newFileUri, Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                                                                InputStream stream =  context.getContentResolver().openInputStream(newFileUri);
+                                                                byte[] byteArray = JasonHelper.readBytes(stream);
+                                                                String encodedString = Base64.encodeToString(byteArray, Base64.DEFAULT);
+                                                                stream.close();
+                                                                newRow.put(rowKey, encodedString);
+                                                            } catch (SecurityException e) {
+                                                                StackTraceElement[] stackTrace = e.getStackTrace();
+                                                                JSONObject errorItem = new JSONObject();
+                                                                errorItem.put(rowKey, rowValue);
+                                                                errorItem.put("_parent_id", row.getString("_id"));
+                                                                errorItem.put("_parent", key);
+                                                                errorItem.put("_id", errorVal.length());
+                                                                errorFullList = valTest;
+                                                                errorVal.put(errorItem);
+                                                                isFaulty = true;
+                                                                newRow.put(rowKey, "Security Error: no access granted for the target file");
+                                                                for (int ii = 0; ii < e.getStackTrace().length; ii++) {
+                                                                    Log.d("warning", stackTrace[ii].getMethodName() + " : " + stackTrace[ii].toString());
+                                                                }                                                            }
+                                                        }
+                                                } else {
+                                                    newRow.put(rowKey, rowValue);
+                                                }
+                                            }
+                                            if (isFaulty) {
+                                                newVal.put(newRow);
+                                            } else {
+                                                newVal.put(newRow);
+                                            }
+                                        }
+                                        Log.d("value builder", "JSONArray : " + valTest.toString());
+                                        bodyBuilder.add(key, newVal.toString());
+                                    } catch (Exception e) {
+                                        String val = d.getString(key);
+                                        Log.d("value builder", "String : " + val);
+                                        bodyBuilder.add(key, val);
+                                    }
                                 }
                             } catch (Exception e) {
                                 Log.d("Warning", e.getStackTrace()[0].getMethodName() + " : " + e.toString());
@@ -184,60 +266,65 @@ public class JasonNetworkAction {
                 } else{
                     client = ((Launcher)context.getApplicationContext()).getHttpClient(0);
                 }
-
-                client.newCall(request).enqueue(new Callback() {
-                    @Override
-                    public void onFailure(Call call, IOException e) {
-                        Timber.e(e);
-                        try {
-                            if (action.has("error")) {
-
-                                JSONObject error = new JSONObject();
-                                error.put("data", e.toString());
-
-                                if(callback != null) {
-                                    JasonHelper.callback(callback, null, context);
-                                } else {
-                                    JasonHelper.next("error", action, error, event, context);
-                                }
-                            }
-                        } catch (Exception e2){
-                            Log.d("Warning", e2.getStackTrace()[0].getMethodName() + " : " + e2.toString());
-                        }
-                    }
-
-                    @Override
-                    public void onResponse(Call call, final Response response) throws IOException {
-                        if (!response.isSuccessful()) {
+                if(errorVal.length() > 0 && action.has("content_error")) {
+                    JSONObject errorObject = new JSONObject();
+                    errorObject.put("content_error", errorVal);
+                    errorObject.put("full_list", errorFullList);
+                    JasonHelper.next("content_error", action, errorObject, event, context);
+                } else {
+                    client.newCall(request).enqueue(new Callback() {
+                        @Override
+                        public void onFailure(Call call, IOException e) {
+                            Timber.e(e);
                             try {
                                 if (action.has("error")) {
+
                                     JSONObject error = new JSONObject();
-                                    error.put("data", response.toString());
-                                    if(callback != null){
+                                    error.put("data", e.toString());
+
+                                    if(callback != null) {
                                         JasonHelper.callback(callback, null, context);
                                     } else {
                                         JasonHelper.next("error", action, error, event, context);
                                     }
                                 }
-                            } catch (Exception e){
-                                Log.d("Warning", e.getStackTrace()[0].getMethodName() + " : " + e.toString());
-                            }
-                        } else {
-                            try {
-                                String jsonData = response.body().string();
-                                if(callback != null){
-                                    JasonHelper.callback(callback, jsonData, context);
-                                } else {
-                                    JasonHelper.next("success", action, jsonData, event, context);
-                                }
-
-                            } catch (Exception e) {
-                                Log.d("Warning", e.getStackTrace()[0].getMethodName() + " : " + e.toString());
+                            } catch (Exception e2){
+                                Log.d("Warning", e2.getStackTrace()[0].getMethodName() + " : " + e2.toString());
                             }
                         }
-                    }
-                });
 
+                        @Override
+                        public void onResponse(Call call, final Response response) throws IOException {
+                            if (!response.isSuccessful()) {
+                                try {
+                                    if (action.has("error")) {
+                                        JSONObject error = new JSONObject();
+                                        error.put("data", response.toString());
+                                        if(callback != null){
+                                            JasonHelper.callback(callback, null, context);
+                                        } else {
+                                            JasonHelper.next("error", action, error, event, context);
+                                        }
+                                    }
+                                } catch (Exception e){
+                                    Log.d("Warning", e.getStackTrace()[0].getMethodName() + " : " + e.toString());
+                                }
+                            } else {
+                                try {
+                                    String jsonData = response.body().string();
+                                    if(callback != null){
+                                        JasonHelper.callback(callback, jsonData, context);
+                                    } else {
+                                        JasonHelper.next("success", action, jsonData, event, context);
+                                    }
+
+                                } catch (Exception e) {
+                                    Log.d("Warning", e.getStackTrace()[0].getMethodName() + " : " + e.toString());
+                                }
+                            }
+                        }
+                    });
+                }
             }
         } catch (Exception e){
             Log.d("Warning", e.getStackTrace()[0].getMethodName() + " : " + e.toString());
